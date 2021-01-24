7e9bff19eb523b65e7cc243bfe75a94d197dc7b2
deraken02 <47181262+deraken02@users.noreply.github.com>
2021-01-24 16:01:11 +0100

    Erreur le dossier n'étais pas censé se trouver là

--- voiture/ecran LCD/ecran/boissons.ino
+++
@@ -1,4 +0,0 @@
-String BOISSONS[]={"Vodka","Rhum","Cointreau","Gin"};
-
-
-String COCKTAILS []={"Mojito","Punch","Blue Lagoon","Long Island","Daikiri","Cuba Libre","Gin Fizz","Zizi Coincoin","Screw Driver","Tom Collins","Cosmopolitain","God Father","Blue Hawaiian","Red Lion","Clover Club","Silver Fizz","Orange Blossom","Balalaïka","Scotch Scour"};
